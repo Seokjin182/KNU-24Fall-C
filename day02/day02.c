f3da723f50a2e2d48934ee95aebeb6735a42d7c3
Seokjin182 <synyn625qwer@gmail.com>
2024-09-12 16:21:13 +0900

    Update day02.c

--- day02/day02.c
+++ day02/day02.c
@@ -5,7 +5,7 @@ int main() {
 	int a;
 	double num1,num2;
 	re:
-	printf("원하는 기능 1.더하기 2.빼기 3.나누기 4.곱하기");
+	printf("원하는 기능 1.더하기 2.빼기 3.나누기 4.곱하기 : ");
 	scanf_s("%d",&a);
 	printf("숫자입력 1 : ");
 	scanf_s("%lf",&num1); 
@@ -17,7 +17,7 @@ int main() {
 	else if(a==3)printf("%f / %f = %f",num1,num2,num1/num2);
 	else if(a==4)printf("%f * %f = %f",num1,num2,num1*num2);
 	else {
-		printf("숫자 다시 입력");
+		printf("숫자 다시 입력\n");
 		goto re; 
 	}
 	return 0;
