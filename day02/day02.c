e97c459dc923cc0f3fb04739f0b86cf2b56f09e7
Seokjin182 <synyn625qwer@gmail.com>
2024-09-12 16:22:42 +0900

    조건문 실습

--- day02/day02.c
+++ day02/day02.c
@@ -5,13 +5,12 @@ int main() {
 	int a;
 	double num1,num2;
 	re:
-	printf("원하는 기능 1.더하기 2.빼기 3.나누기 4.곱하기 : ");
+	printf("원하는 기능 1.더하기 2.빼기 3.나누기 4.곱하기:");
 	scanf_s("%d",&a);
 	printf("숫자입력 1 : ");
 	scanf_s("%lf",&num1); 
 	printf("숫자입력 2 : ");
 	scanf_s("%lf",&num2);
-	
 	if(a==1)printf("%f + %f = %f",num1,num2,num1+num2);
 	else if(a==2)printf("%f - %f = %f",num1,num2,num1-num2);
 	else if(a==3)printf("%f / %f = %f",num1,num2,num1/num2);
