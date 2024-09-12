aed60773264460e9a5ed422cf479b6cd0fcad6b9
Seokjin182 <synyn625qwer@gmail.com>
2024-09-12 17:07:38 +0900

    day02-3 반복문 실습

---
+++ day02/day02-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+	int a,b;
+	for(a=0;a<=99;a++){
+		if(a%3==0&&a%4==0&&a%7==0){
+			printf("%d",a);
+			a=a+1;
+		}
+		else if(a%3==0&&a%4==0){
+			printf("%d ",a);
+			
+		}
+		else if(a%7==0)printf("%d\n",a);		
+	}	
+	return 0;
+}
