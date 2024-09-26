108bab2ed3c3045a916c89b909fcc329e6af9fc5
Seokjin182 <synyn625qwer@gmail.com>
2024-09-26 17:47:30 +0900

    포인터

--- day05/day05-1.c
+++ day05/day05-1.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct Student{
+	int snum;
+	char name[100];
+	int score;
+};
 
 int main() {
+	struct Student *p;
+	int i,n,max,min,avg;
+		
+	printf("학생수 입력");
+	scanf_s("%d",&n);
+	p=(struct Student*)malloc(n*sizeof(struct Student));
+
+	
+	for(i=0;i<n;i++){
+		printf("학번 : "); 
+		scanf_s("%d",&p[i].snum);
+		printf("이름 : ");
+		scanf_s("%s",p[i].name,100);
+		printf("점수 : ");
+		scanf_s("%d",&p[i].score);
+		
+	}
+	avg=avg/n;
+	
+	printf("최대값 : %d\n",max);
+	printf("최소값 : %d\n",min);
+	printf("평균값 : %d\n",avg);
+	
+	free(p);
+	p=NULL;
+	
+	
 	return 0;
 }
