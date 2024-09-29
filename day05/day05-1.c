f2f0f73db388b8801c144810571d30d380bbc117
Seokjin182 <synyn625qwer@gmail.com>
2024-09-29 17:26:25 +0900

    struct

--- day05/day05-1.c
+++ day05/day05-1.c
@@ -9,7 +9,7 @@ struct Student{
 
 int main() {
 	struct Student *p;
-	int i,n,max,min,avg;
+	int i,n,max=0,min,avg=0;
 		
 	printf("학생수 입력");
 	scanf_s("%d",&n);
@@ -23,10 +23,12 @@ int main() {
 		scanf_s("%s",p[i].name,100);
 		printf("점수 : ");
 		scanf_s("%d",&p[i].score);
-		
+		if(max<p[i].score)max=p[i].score;
+		if(i==0)min=p[i].score;
+		if(min>p[i].score)min=p[i].score;
+		avg=avg+p[i].score;
 	}
 	avg=avg/n;
-	
 	printf("최대값 : %d\n",max);
 	printf("최소값 : %d\n",min);
 	printf("평균값 : %d\n",avg);
