9387b4ab56094703afcf1e4d1e55eff621504ef7
Seokjin182 <synyn625qwer@gmail.com>
2024-10-10 14:24:19 +0900

    몬테카를로

--- day07/day07-2.c
+++ day07/day07-2.c
@@ -6,7 +6,7 @@
 int main(){
 	double x = 0;
 	double y = 0;
-	int count=0;
+	int count=0,a=0,i;
 	double cir=0;
 	srand(time(NULL));
 	
@@ -17,7 +17,17 @@ int main(){
 		if((x*x)+(y*y)<=1){
 			cir++;
 		}
-		if(count%5000000==0)printf("현재 진행 상황%d%% pi=%f\n",(count/5000000)*5,(cir/count)*4);
+		if(count%1000000==0){
+			printf("현재 진행 상황%d%% pi=%f",(count/1000000),(cir/count)*4);
+			if(count%5000000==0){
+				a++;
+			}
+			for(i=0;i<=a;i++){
+					printf("*");	
+			}
+			printf("\n");
+		}
+		
 	}
 	
 	return 0;
