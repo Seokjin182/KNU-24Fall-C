954da9972179c7c8588206c16abf8bca4ef3fef9
Seokjin182 <synyn625qwer@gmail.com>
2024-12-01 01:07:32 +0900

    점프,순간이동

---
+++ day12/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int solution(int n)
+{
+    int k = 0;
+    while(n>0){
+        if(n%2==0){
+            n=n/2;
+        }
+        else {
+            n--;
+            k++;
+        }
+    }
+
+    return k;
+}
